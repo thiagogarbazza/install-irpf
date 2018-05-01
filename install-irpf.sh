41a2a76736ecbc43acc3a281fea364b80dffe638
thiagogarbazza <thiagogarbazza@gmail.com>
2018-05-01 09:53:33 -0300

    Adicionando permissão de leitura para todos os usuários no diretório de instalação. adicionando mensagens durante a instalação.

--- install-irpf.sh
+++ install-irpf.sh
@@ -1,13 +1,22 @@
-sudo rm -Rf /opt/irpf*
-sudo rm -Rf /usr/share/applications/irpf*.desktop
+echo "Removendo outras versões do aplicativo"
 
+sudo rm -Rf /opt/irpf
+sudo rm -Rf /usr/share/applications/irpf.desktop
+
+echo "Realizar o download do aplicativo"
 wget http://downloadirpf.receita.fazenda.gov.br/irpf/2018/irpf/arquivos/IRPF2018-1.4.zip -O irpf.zip
 
-sudo unzip irpf.zip -d /opt/
+echo "Instalando o aplicativo em /opt/irpf"
+sudo unzip irpf.zip -d /opt
 sudo mv /opt/IRPF*/ /opt/irpf
 sudo cp irpf-icon.png /opt/irpf/irpf.png
+sudo chmod -R +r /opt/irpf
 
+echo "Criando link para acessi fácil"
 sudo cp irpf-link.desktop /usr/share/applications/irpf.desktop
-sudo chmod +x /usr/share/applications/irpf.desktop
+sudo chmod +rx /usr/share/applications/irpf.desktop
 
 rm -Rf irpf.zip
+
+
+echo "Instalação concluida com sucesso!"
