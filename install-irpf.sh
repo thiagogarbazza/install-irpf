6ce2a594ed083281ff2b1a18f36730299cca776f
Thiago Garbazza <thiagogarbazza@gmail.com>
2020-03-19 17:20:12 -0300

    Atualizando para instalar a versão 2020

--- install-irpf.sh
+++ install-irpf.sh
@@ -5,7 +5,8 @@ sudo rm -Rf /usr/share/applications/irpf.desktop
 
 echo "Realizar o download do aplicativo"
 #wget http://downloadirpf.receita.fazenda.gov.br/irpf/2018/irpf/arquivos/IRPF2018-1.4.zip -O irpf.zip
-wget http://downloadirpf.receita.fazenda.gov.br/irpf/2019/irpf/arquivos/IRPF2019-1.5.zip -O irpf.zip
+#wget http://downloadirpf.receita.fazenda.gov.br/irpf/2019/irpf/arquivos/IRPF2019-1.6.zip -O irpf.zip
+wget http://downloadirpf.receita.fazenda.gov.br/irpf/2020/irpf/arquivos/IRPF2020-1.5.zip -O irpf.zip
 
 echo "Instalando o aplicativo em /opt/irpf"
 sudo unzip irpf.zip -d /opt
